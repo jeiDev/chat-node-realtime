7fb3475a43ac5bacf2ef1ad397d5fb8d44fc63a1
JeiDevp <joseanyeloserrano@gmail.com>
2019-03-03 16:38:40 -0400

    Complete connetion socke in client of server

--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,9 @@
 function routes() {
 	const express = require("express"),
-	      app = express(),
-		  http = require('http').Server(app),
-		  path = require("path"),
-		  io = require("socket.io")(http);
+		app = express(),
+		http = require('http').Server(app),
+		path = require("path"),
+		io = require("socket.io")(http);
 
 	app.use(express.static('public'))
 	app.get('/', function (req, res) {
@@ -16,10 +16,22 @@ function routes() {
 		res.sendFile(path.join(url));
 	});
 
-    io.on('connection', function(socket){
-        console.log("User connected")
-    })
-	
+	io.on('connection', function (socket) {
+		console.log("User connected")
+
+		socket.on('message', function (msg) {
+			io.emit('message', msg);
+			console.log('message: ' + msg);
+		});
+
+		//socket.broadcast.emit('hi');
+
+		socket.on('disconnect', function () {
+			console.log('user disconnected');
+		});
+
+	})
+
 	http.listen(80, function () {
 		console.log('listening on *:80');
 	});
