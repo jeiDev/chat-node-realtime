2c4c8ea062bd493b60312069c0ddddac6fedbb64
JeiDevp <joseanyeloserrano@gmail.com>
2019-03-03 22:12:54 -0400

    Connect login in API and  alert error

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,3 +1,5 @@
+var alertLoginActive = false;
+
 document.body.onload = init
 
 function init() {
@@ -6,7 +8,8 @@ function init() {
      ********************/
     let contentMove = document.getElementById("contentMove"),
         contentRegister = document.getElementById("contentRegister"),
-        contentLogin = document.getElementById("contentLogin");
+        contentLogin = document.getElementById("contentLogin"),
+        onClickLogin = false;
 
     document.getElementById("showSignUp").addEventListener("click", () => {
         contentMove.classList.add("animation-left-minus")
@@ -26,5 +29,66 @@ function init() {
             contentMove.style.left = "460px"
             contentMove.classList.remove("animation-left-plus")
         }, 600)
+    })
+
+    document.getElementById("login").addEventListener("click", () => {
+        if (onClickLogin) return
+        onClickLogin = true
+
+        let email = document.getElementById("emailLogin").value,
+            pass = document.getElementById("passLogin").value,
+            barEmailLogin = document.getElementById("barEmailLogin"),
+            barPassLogin = document.getElementById("barPassLogin"),
+            emptyLogin = document.getElementById("emptyLogin");
+
+        if (email == "") barEmailLogin.style.background = "hsla(0, 100%, 51%, 0.63)"
+        else barEmailLogin.style.background = "hsla(214, 100%, 51%, 0.63)"
+
+        if (pass == "") barPassLogin.style.background = "hsla(0, 100%, 51%, 0.63)"
+        else barPassLogin.style.background = "hsla(214, 100%, 51%, 0.63)"
+
+
+        if (email == "" || pass == "") {
+            if (alertLoginActive) {
+                onClickLogin = false
+                return
+            }
+            emptyLogin.style.top = "0"
+
+            setTimeout(() => {
+                emptyLogin.style.top = "-40px"
+                alertLoginActive = false
+                onClickLogin = false
+            }, 2000)
+            return
+        }
+
+        login(email, pass)
+        onClickLogin = false
+    })
+}
+
+function login(email, pass) {
+    let data = {
+        type: "POST",
+        data: {email, password: pass},
+        url: `${api}Users/login`,
+    }
+
+    provider(data).then(res => {
+        
+    }).catch(err => {
+        if(err.error.code == "LOGIN_FAILED"){
+            if(!alertLoginActive){
+                alertLoginActive = true
+                let incorrectCredentials = document.getElementById("incorrectCredentials")
+                incorrectCredentials.style.top = "0"
+
+                setTimeout(() => {
+                    incorrectCredentials.style.top = "-40px"
+                    alertLoginActive = false
+                }, 2000)
+            }
+        }
     })
 }
---
+++ public/assets/js/main.js
@@ -0,0 +1,20 @@
+var api = "http://192.168.8.104:3000/api/",
+    token = localStorage.session ? JSON.parse(localStorage.session).token : null
+
+/*
+* @param {Object} send data in type Object example {type: "POST" (Required), url: "http://example.com"(Required), data: "Data to send" (Optional), token: "Ad2s5z5d6asdDSd5kdas6das5" (Optional)}
+*/
+function provider(data) {
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest()
+
+        xhr.open(data.type, data.url)
+        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
+        xhr.onload = () => {
+            if (xhr.status == 200) resolve(JSON.parse(xhr.responseText))
+            else reject(JSON.parse(xhr.responseText))
+        }
+
+        xhr.send(`${data.data ? JSON.stringify(data.data) : null}`)
+    })
+}
